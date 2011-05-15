99c8315d17aaaac678199ee9610cabb4a68fdf68
lyudmil.pelov <lyudmil.pelov@1db86f74-f416-6f16-c632-b9b7d900f8b9>
2011-05-15 14:18:31 +0000

    add license

--- trunk/gwt2go/src/com/gwt2go/dev/client/Gwt2go.java
+++ trunk/gwt2go/src/com/gwt2go/dev/client/Gwt2go.java
@@ -1,10 +1,26 @@
+/*
+ * Copyright (C) 2011  Lyudmil Pelov
+ * TestFactory- Web Based System for rating and control student's knowledge.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
 package com.gwt2go.dev.client;
 
 import com.google.gwt.core.client.EntryPoint;
 
 /**
- * @author L.Pelov
- * Entry point classes define <code>onModuleLoad()</code>.
+ * @author L.Pelov Entry point classes define <code>onModuleLoad()</code>.
  */
 public class Gwt2go implements EntryPoint {
 	/**
