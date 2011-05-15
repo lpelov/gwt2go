55288568aba367ba08c7ffc73b5650ae81131d8c
lyudmil.pelov <lyudmil.pelov@1db86f74-f416-6f16-c632-b9b7d900f8b9>
2011-05-15 14:04:06 +0000

    Init gwt2go project

---
+++ trunk/gwt2go/src/com/gwt2go/dev/client/Gwt2go.java
@@ -0,0 +1,16 @@
+package com.gwt2go.dev.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * @author L.Pelov
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class Gwt2go implements EntryPoint {
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() {
+		// GWT Entry Point Module
+	}
+}
