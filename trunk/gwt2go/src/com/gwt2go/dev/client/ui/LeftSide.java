5034c85ad3a5fbbc89897ea9298f313727581b47
lyudmil.pelov@gmail.com <lyudmil.pelov@gmail.com@1db86f74-f416-6f16-c632-b9b7d900f8b9>
2012-01-28 09:49:23 +0000

    add tree view example

--- trunk/gwt2go/src/com/gwt2go/dev/client/ui/LeftSide.java
+++ trunk/gwt2go/src/com/gwt2go/dev/client/ui/LeftSide.java
@@ -1,5 +1,19 @@
+/*
+ * Copyright 2010
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.gwt2go.dev.client.ui;
-
 
 import com.google.gwt.cell.client.TextCell;
 import com.google.gwt.core.client.GWT;
@@ -8,11 +22,14 @@ import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.cellview.client.CellTree;
+import com.google.gwt.user.cellview.client.HasKeyboardSelectionPolicy.KeyboardSelectionPolicy;
+import com.google.gwt.user.cellview.client.TreeNode;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.view.client.ListDataProvider;
 import com.google.gwt.view.client.TreeViewModel;
+import com.gwt2go.dev.client.data.CustomTreeModel;
 import com.gwt2go.dev.client.place.RootPlace;
 import com.gwt2go.dev.client.ClientFactory;
 
@@ -26,51 +43,69 @@ public class LeftSide extends Composite {
 
 	@UiField
 	Button button1;
-	
+
 	@UiField
 	Button button2;
-	
+
 	@UiField
 	Button button3;
 
 	@UiField
 	Button btnEditor;
 
-	@UiField(provided=true) CellTree cellTree = new CellTree(
-		new TreeViewModel() {
-//			final AbstractDataProvider<String> dataProvider = new ListDataProvider<String>();
-//			final AbstractSelectionModel<String> selectionModel = new NoSelectionModel<String>();
-			@Override
-			public <T> NodeInfo<?> getNodeInfo(T value) {
-			      /*
-			       * Create some data in a data provider. Use the parent value as a prefix
-			       * for the next level.
-			       */
-			      ListDataProvider<String> dataProvider = new ListDataProvider<String>();
-			      for (int i = 0; i < 2; i++) {
-			        dataProvider.getList().add(value + "." + String.valueOf(i));
-			      }
+	@UiField(provided = true)
+	CellTree cellTree = new CellTree(new TreeViewModel() {
+		// final AbstractDataProvider<String> dataProvider = new
+		// ListDataProvider<String>();
+		// final AbstractSelectionModel<String> selectionModel = new
+		// NoSelectionModel<String>();
+		@Override
+		public <T> NodeInfo<?> getNodeInfo(T value) {
+			/*
+			 * Create some data in a data provider. Use the parent value as a
+			 * prefix for the next level.
+			 */
+			ListDataProvider<String> dataProvider = new ListDataProvider<String>();
+			for (int i = 0; i < 2; i++) {
+				dataProvider.getList().add(value + "." + String.valueOf(i));
+			}
 
-			      // Return a node info that pairs the data with a cell.
-			      return new DefaultNodeInfo<String>(dataProvider, new TextCell());
-				//return new DefaultNodeInfo<String>(dataProvider, new TextCell(), selectionModel, null);
-			}
-			@Override
-			public boolean isLeaf(Object value) {
-				return value.toString().length() > 10;
-				//return true;
-			}
-		}, "Item 1");
+			// Return a node info that pairs the data with a cell.
+			return new DefaultNodeInfo<String>(dataProvider, new TextCell());
+			// return new DefaultNodeInfo<String>(dataProvider, new TextCell(),
+			// selectionModel, null);
+		}
+
+		@Override
+		public boolean isLeaf(Object value) {
+			return value.toString().length() > 10;
+			// return true;
+		}
+	}, "Item 1");
+
+	@UiField(provided = true)
+	CellTree cellTree2;
 
 	ClientFactory clientFactory;
-	
+
 	public LeftSide(ClientFactory clientFactory) {
+		TreeViewModel model = new CustomTreeModel();
+
+		// add the new tree
+		cellTree2 = new CellTree(model, null);
+		cellTree2.setKeyboardSelectionPolicy(KeyboardSelectionPolicy.ENABLED);
+
+		//
+		TreeNode rootNode = cellTree2.getRootTreeNode();
+		TreeNode firstPlaylist = rootNode.setChildOpen(0, true);
+		firstPlaylist.setChildOpen(0, true);
+
 		initWidget(uiBinder.createAndBindUi(this));
 		button1.setText("SortingTable1");
 		button2.setText("SortingTable2");
 		button3.setText("SortingTable_GWT2.3");
 		btnEditor.setText("EditorWS");
-		
+
 		this.clientFactory = clientFactory;
 		this.cellTree.setAnimationEnabled(true);
 	}
@@ -87,7 +122,8 @@ public class LeftSide extends Composite {
 
 	@UiHandler("button3")
 	void onButton3Click(ClickEvent e) {
-		this.clientFactory.getPlaceController().goTo(new RootPlace("sortingtable23"));
+		this.clientFactory.getPlaceController().goTo(
+				new RootPlace("sortingtable23"));
 	}
 
 	@UiHandler("btnEditor")
