a68ce81e17cb94221a2d3b8c9380bf75e8534709
lyudmil.pelov@gmail.com <lyudmil.pelov@gmail.com@1db86f74-f416-6f16-c632-b9b7d900f8b9>
2012-04-06 11:21:47 +0000

    new cell tree view example

--- trunk/gwt2go/src/com/gwt2go/dev/client/ui/view/CellTreeExpl1View.java
+++ trunk/gwt2go/src/com/gwt2go/dev/client/ui/view/CellTreeExpl1View.java
@@ -1,8 +1,27 @@
+/*
+ * Copyright 2010, L.Pelov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.gwt2go.dev.client.ui.view;
 
 import com.google.gwt.place.shared.Place;
 import com.google.gwt.user.client.ui.IsWidget;
 
+/**
+ * 
+ * @author L.Pelov
+ */
 public interface CellTreeExpl1View extends IsWidget {
 
 	void setName(String tableName);
