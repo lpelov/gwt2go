ddd85c26218f892273ffedff8fbe73268cdd9a01
lyudmil.pelov@gmail.com <lyudmil.pelov@gmail.com@1db86f74-f416-6f16-c632-b9b7d900f8b9>
2012-04-07 18:53:20 +0000

    lazy panel with simple native GWT drag and drop example

--- trunk/gwt2go/src/com/gwt2go/dev/client/ui/widget/LazySimplePanel.java
+++ trunk/gwt2go/src/com/gwt2go/dev/client/ui/widget/LazySimplePanel.java
@@ -15,42 +15,88 @@
  */
 package com.gwt2go.dev.client.ui.widget;
 
+import com.google.gwt.event.dom.client.DragOverEvent;
+import com.google.gwt.event.dom.client.DragOverHandler;
 import com.google.gwt.event.dom.client.DragStartEvent;
 import com.google.gwt.event.dom.client.DragStartHandler;
+import com.google.gwt.event.dom.client.DropEvent;
+import com.google.gwt.event.dom.client.DropHandler;
 import com.google.gwt.user.client.Element;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.LazyPanel;
 import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 
 public class LazySimplePanel extends LazyPanel {
 
 	private Element nameSpan;
+	private VerticalPanel vrPanel = new VerticalPanel();
 	
 	public LazySimplePanel(Element nameSpan) {
 		this.nameSpan = nameSpan;
 	}
-	
+
 	@Override
 	protected Widget createWidget() {
 		SimplePanel panel = new SimplePanel();
-		
+
 		final Label label = new Label("Drag me");
 		label.getElement().setDraggable(Element.DRAGGABLE_TRUE);
-		
+
 		label.addDragStartHandler(new DragStartHandler() {
-			
+
 			@Override
 			public void onDragStart(DragStartEvent event) {
 				// required
 				event.setData("text", "Hello World");
-				
+
 				// optinal: show copy of the image
-				event.getDataTransfer().setDragImage(label.getElement(), 10, 10);
+				event.getDataTransfer()
+						.setDragImage(label.getElement(), 10, 10);
 			}
 		});
+
+		// some widgets do not implement drag handles
+		// for that reason you can use addDomHandler()
+		/* label.addDomHandler(new DragStartHandler() {
+			@Override
+			public void onDragStart(DragStartEvent event) {
+				// required
+				event.setData("text", "Hello World again;)");
+				
+				// optional
+				event.getDataTransfer().setDragImage(label.getElement(), 10, 10);
+			}
+		}, DragStartEvent.getType()); */
 		
-		panel.getElement().appendChild(this.nameSpan);
+		
+		final Label target = new Label("Drag onto me");
+		// required: you must add dragoverhandler to create a target
+		target.addDragOverHandler(new DragOverHandler() {
+			@Override
+			public void onDragOver(DragOverEvent event) {
+				target.getElement().getStyle().setBackgroundColor("#ffa");
+			}
+		});
+
+		// add drop hanlder
+		target.addDropHandler(new DropHandler() {
+			@Override
+			public void onDrop(DropEvent event) {
+				// prevent the native text drop
+				event.preventDefault();
+
+				// get the data out of the event
+				String data = event.getData("text");
+				target.setText(data);
+			}
+		});
+
+		panel.add(vrPanel);
+		vrPanel.getElement().appendChild(this.nameSpan);
+		vrPanel.add(label);
+		vrPanel.add(target);
 		return panel;
 	}
 
