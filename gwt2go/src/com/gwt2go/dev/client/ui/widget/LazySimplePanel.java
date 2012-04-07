8392b019295abff431207a1121bd29c93e527bea
lyudmil.pelov@gmail.com <lyudmil.pelov@gmail.com@1db86f74-f416-6f16-c632-b9b7d900f8b9>
2012-04-07 18:34:04 +0000

    start to add drag and drop example

--- gwt2go/src/com/gwt2go/dev/client/ui/widget/LazySimplePanel.java
+++ gwt2go/src/com/gwt2go/dev/client/ui/widget/LazySimplePanel.java
@@ -15,7 +15,10 @@
  */
 package com.gwt2go.dev.client.ui.widget;
 
+import com.google.gwt.event.dom.client.DragStartEvent;
+import com.google.gwt.event.dom.client.DragStartHandler;
 import com.google.gwt.user.client.Element;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.LazyPanel;
 import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.Widget;
@@ -31,6 +34,22 @@ public class LazySimplePanel extends LazyPanel {
 	@Override
 	protected Widget createWidget() {
 		SimplePanel panel = new SimplePanel();
+		
+		final Label label = new Label("Drag me");
+		label.getElement().setDraggable(Element.DRAGGABLE_TRUE);
+		
+		label.addDragStartHandler(new DragStartHandler() {
+			
+			@Override
+			public void onDragStart(DragStartEvent event) {
+				// required
+				event.setData("text", "Hello World");
+				
+				// optinal: show copy of the image
+				event.getDataTransfer().setDragImage(label.getElement(), 10, 10);
+			}
+		});
+		
 		panel.getElement().appendChild(this.nameSpan);
 		return panel;
 	}
