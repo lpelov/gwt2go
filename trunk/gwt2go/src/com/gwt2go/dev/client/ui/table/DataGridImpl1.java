2f2cd35d933a46ad55ce6baf50d0e079c801c1fa
lyudmil.pelov <lyudmil.pelov@1db86f74-f416-6f16-c632-b9b7d900f8b9>
2012-05-29 20:00:36 +0000

    initial version of datagrid example, which will be extended later

--- trunk/gwt2go/src/com/gwt2go/dev/client/ui/table/DataGridImpl1.java
+++ trunk/gwt2go/src/com/gwt2go/dev/client/ui/table/DataGridImpl1.java
@@ -1,127 +1,141 @@
+/*
+ * Copyright 2012, L.Pelov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.gwt2go.dev.client.ui.table;
 
 import java.util.Arrays;
-import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 
+import com.google.gwt.cell.client.DateCell;
 import com.google.gwt.dom.client.Element;
-import com.google.gwt.user.cellview.client.CellTable;
-import com.google.gwt.user.cellview.client.ColumnSortEvent.ListHandler;
+import com.google.gwt.user.cellview.client.Column;
+import com.google.gwt.user.cellview.client.DataGrid;
+import com.google.gwt.user.cellview.client.HasKeyboardSelectionPolicy.KeyboardSelectionPolicy;
 import com.google.gwt.user.cellview.client.SimplePager;
 import com.google.gwt.user.cellview.client.TextColumn;
 import com.google.gwt.user.client.DOM;
-import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.SimplePanel;
-import com.google.gwt.view.client.ListDataProvider;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.LayoutPanel;
+import com.google.gwt.user.client.ui.ResizeComposite;
+import com.google.gwt.view.client.SelectionChangeEvent;
+import com.google.gwt.view.client.SingleSelectionModel;
 
-public class DataGridImpl1 extends Composite implements DataGridView {
+public class DataGridImpl1 extends ResizeComposite implements DataGridView {
 
-	SimplePanel viewPanel = new SimplePanel();
+	LayoutPanel viewPanel = new LayoutPanel();
 	Element nameSpan = DOM.createSpan();
 	Presenter presenter;
 	SimplePager pager;
-	
+
 	// A simple data type that represents a contact.
 	private static class Contact {
 		private final String address;
 		private final String name;
-		private final String color;
+		private final Date birthday;
 
-		public Contact(String name, String address, String color) {
+		public Contact(String name, Date birthday, String address, String color) {
 			this.name = name;
 			this.address = address;
-			this.color = color;
+			this.birthday = birthday;
 		}
 	}
 
+	// TODO: This is not going to work like this, see CellTableSortingViewImpl
+	// TODO: check to see the layouts, should use LayoutPanel, which means you
+	// have to use RootLayoutPanel
+	
 	// The list of data to display.
-	private static List<Contact> CONTACTS = Arrays.asList(new Contact("John",
-			"123 Fourth Road", "red;"), new Contact("Mary", "222 Lancer Lane", "green;"),
-			new Contact("Zander", "94 Road Street", "blue;"));
+	@SuppressWarnings("deprecation")
+	private static List<Contact> CONTACTS = Arrays
+			.asList(new Contact("John", new Date(80, 4, 12), "123 Fourth Road",
+					"red;"), new Contact("Mary", new Date(85, 2, 22),
+					"222 Lancer Lane", "green;"), new Contact("Zander",
+					new Date(46, 6, 6), "94 Road Street", "blue;"));
 
 	public DataGridImpl1() {
-		// -- START TABLE
-	    // Create a CellTable.
-	    CellTable<Contact> table = new CellTable<Contact>();
 
-	    // Create name column.
-	    TextColumn<Contact> nameColumn = new TextColumn<Contact>() {
-	      @Override
-	      public String getValue(Contact contact) {
-	        return contact.name;
-	      }
-	    };
+		// Create a CellTable.
+		DataGrid<Contact> table = new DataGrid<Contact>();
+		table.setWidth("100%");
+		table.setHeight("100px");
+		table.setEmptyTableWidget(new Label("No Information to show"));
+		table.setKeyboardSelectionPolicy(KeyboardSelectionPolicy.ENABLED);
 
-	    // Make the name column sortable.
-	    nameColumn.setSortable(true);
+		// Add a text column to show the name.
+		TextColumn<Contact> nameColumn = new TextColumn<Contact>() {
+			@Override
+			public String getValue(Contact object) {
+				return object.name;
+			}
+		};
+		table.addColumn(nameColumn, "Name");
 
-	    // Create address column.
-	    TextColumn<Contact> addressColumn = new TextColumn<Contact>() {
-	      @Override
-	      public String getValue(Contact contact) {
-	        return contact.address;
-	      }
-	    };
+		// Add a date column to show the birthday.
+		DateCell dateCell = new DateCell();
+		Column<Contact, Date> dateColumn = new Column<Contact, Date>(dateCell) {
+			@Override
+			public Date getValue(Contact object) {
+				return object.birthday;
+			}
+		};
 
-	    ImagesColumn<Contact> imagesColumn = new ImagesColumn<Contact>(){
-	    	@Override
-	    	public String getValue(Contact object) {	    		
-	    		return object.color;
-	    	}
-	    };
-	    
-	    
-	    // Add the columns.
-	    table.addColumn(nameColumn, "Name");
-	    table.addColumn(addressColumn, "Address");
-	    table.addColumn(imagesColumn, "Export to");
-	    
+		table.addColumn(dateColumn, "Birthday");
 
-	    // Create a data provider.
-	    ListDataProvider<Contact> dataProvider = new ListDataProvider<Contact>();
+		// Add a text column to show the address.
+		TextColumn<Contact> addressColumn = new TextColumn<Contact>() {
+			@Override
+			public String getValue(Contact object) {
+				return object.address;
+			}
+		};
+		table.addColumn(addressColumn, "Address");
 
-	    // Connect the table to the data provider.
-	    dataProvider.addDataDisplay(table);
+		// Add a selection model to handle user selection.
+		final SingleSelectionModel<Contact> selectionModel = new SingleSelectionModel<Contact>();
+		table.setSelectionModel(selectionModel);
+		selectionModel
+				.addSelectionChangeHandler(new SelectionChangeEvent.Handler() {
+					public void onSelectionChange(SelectionChangeEvent event) {
+						Contact selected = selectionModel.getSelectedObject();
+						if (selected != null) {
+							Window.alert("You selected: " + selected.name);
+						}
+					}
+				});
 
-	    // Add the data to the data provider, which automatically pushes it to the
-	    // widget.
-	    List<Contact> list = dataProvider.getList();
-	    for (Contact contact : CONTACTS) {
-	      list.add(contact);
-	    }
+		// Set the total row count. This isn't strictly necessary, but it
+		// affects
+		// paging calculations, so its good habit to keep the row count up to
+		// date.
+		table.setRowCount(CONTACTS.size(), true);
 
-	    // Add a ColumnSortEvent.ListHandler to connect sorting to the
-	    // java.util.List.
-	    ListHandler<Contact> columnSortHandler = new ListHandler<Contact>(
-	        list);
-	    columnSortHandler.setComparator(nameColumn,
-	        new Comparator<Contact>() {
-	          public int compare(Contact o1, Contact o2) {
-	            if (o1 == o2) {
-	              return 0;
-	            }
-
-	            // Compare the name columns.
-	            if (o1 != null) {
-	              return (o2 != null) ? o1.name.compareTo(o2.name) : 1;
-	            }
-	            return -1;
-	          }
-	        });
-	    table.addColumnSortHandler(columnSortHandler);
-
-	    // We know that the data is sorted alphabetically by default.
-	    table.getColumnSortList().push(nameColumn);
+		// Push the data into the widget.
+		table.setRowData(0, CONTACTS);
+		
 
 		// -- END TABLE
 
-		viewPanel.getElement().appendChild(nameSpan);
+		// viewPanel.getElement().appendChild(nameSpan);
 
 		viewPanel.add(table);
+		viewPanel.setSize("30em", "10em");
 		
 		initWidget(viewPanel);
-		
-		
+
 	}
 
 	@Override
